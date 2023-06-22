4fe373e3941f99d63d76215751a3ee3e86fd5f2c
Adiyan Baig <126752583+Adiyanbaig01@users.noreply.github.com>
2023-06-22 17:26:04 +0530

    Update Staccato.js

--- Staccato.js
+++ Staccato.js
@@ -69,6 +69,34 @@ let songs = [
     name: 'Apna Bana Le'
   },
   {
+    url: 'Songs/hawayein.mp3',
+    name: 'Arijit Singh - Hawayein'
+  },
+  {
+    url: 'Songs/Ik Vaari Aa.mp3',
+    name: 'Ek Vaari Aa'
+  },
+  {
+    url: 'Songs/Khairiyat.mp3',
+    name: 'Khairiyat'
+  },
+  {
+    url: 'Songs/Kesariya.mp3',
+    name: 'Arijit Singh - Kesariya'
+  },
+  {
+    url: 'Songs/Shayad.mp3',
+    name: ' Arijit Singh - Shayad '
+  },
+  {
+    url: 'Songs/Zaalima.mp3',
+    name: 'Arijit Singh - Zaalima '
+  },
+  {
+    url: 'Songs/Arijit Singh_ Phir Aur Kya Chahiye.mp3',
+    name: 'Phir Aur Kya Chahiye'
+  },
+  {
     url: 'Songs/BLACKPINK - Dont know what to do MV.mp3',
     name: 'Dont know what to do'
   },
@@ -115,18 +143,18 @@ function playSong(songUrl, songName) {
     audio.play();
   }
   currentAudio = audio;
-  
+
   // finds the index of the currently playing song in the songs array and updates the currentSongIndex
-  currentSongIndex = songs.findIndex(song => song.url === songUrl); 
+  currentSongIndex = songs.findIndex(song => song.url === songUrl);
 
   audio.addEventListener('ended', playNextSong);
-  
+
 
   document.getElementById('songName').textContent = songName;
 
-  
 
-  audio.addEventListener('loadedmetadata', function() {
+
+  audio.addEventListener('loadedmetadata', function () {
     // Get the duration of the audio in seconds
     const duration = audio.duration;
 
@@ -160,9 +188,9 @@ function playSong(songUrl, songName) {
 
   // Call the displayCurrentTime function periodically
   intervalId = setInterval(displayCurrentTime, 100);
-  
-   // Reset the isFirstLoad flag after the first load
-   isFirstLoad = false;
+
+  // Reset the isFirstLoad flag after the first load
+  isFirstLoad = false;
 }
 
 function togglePlayPause() {
@@ -218,29 +246,29 @@ function updatePlayPauseIcon(state) {
     playPauseIcon.src = 'assets/Play.svg';
   }
 }
- // Add the event listener for the progressBar input event
-    progressBarVolume.addEventListener('input', function() {
-      const volume = progressBarVolume.value / 100;
-      currentAudio.volume = volume;
-    });
+// Add the event listener for the progressBar input event
+progressBarVolume.addEventListener('input', function () {
+  const volume = progressBarVolume.value / 100;
+  currentAudio.volume = volume;
+});
 
-    // Add the event listener for the progressBar change event
-    progressBarVolume.addEventListener('change', function() {
-      const volume = progressBarVolume.value / 100;
-      currentAudio.volume = volume;
+// Add the event listener for the progressBar change event
+progressBarVolume.addEventListener('change', function () {
+  const volume = progressBarVolume.value / 100;
+  currentAudio.volume = volume;
 
-      if (progressBarVolume.value == 0) {
-        // Mute the audio
-        currentAudio.muted = true;
-      } else {
-        // Unmute the audio
-        currentAudio.muted = false;
-      }
-    });
+  if (progressBarVolume.value == 0) {
+    // Mute the audio
+    currentAudio.muted = true;
+  } else {
+    // Unmute the audio
+    currentAudio.muted = false;
+  }
+});
 
 
 
-    // SLider Logic for the Website 
+// SLider Logic for the Website 
 
 // Get the song list container element
 const songListContainer = document.querySelector('.list');
@@ -300,104 +328,104 @@ nextButton.addEventListener('click', () => {
 
 
 
- // Program for The Droplets 
+// Program for The Droplets 
 
- let drops = [];
- let thunderTime = 0;
- let sparklines = [];
- let backgroundBrightness = 0;
- let isRaining = true;
- let rainToggle;
- 
- function setup() {
-   createCanvas(windowWidth, windowHeight);
-   for (let i = 0; i < 100; i++) {
-     drops.push(new Drop());
-   }
- 
-   rainToggle = document.getElementById('rainToggle');
-   rainToggle.addEventListener('change', toggleRain);
- }
- 
- function draw() {
-   background(backgroundBrightness);
- 
-   if (isRaining) {
-     for (let i = 0; i < drops.length; i++) {
-       drops[i].fall();
-       drops[i].display();
-     }
- 
-     if (millis() - thunderTime > random(5000, 6000)) {
-       thunderTime = millis();
-       sparklines.push(new Sparkline());
-       backgroundBrightness = 100;
-     }
- 
-     for (let i = sparklines.length - 1; i >= 0; i--) {
-       sparklines[i].display();
-       if (sparklines[i].isFinished()) {
-         sparklines.splice(i, 1);
-       }
-     }
- 
-     backgroundBrightness -= 1;
-   }
- }
- 
- class Drop {
-   constructor() {
-     this.x = random(width);
-     this.y = random(-500, -50);
-     this.z = random(0, 20);
-     this.len = map(this.z, 0, 20, 10, 30);
-     this.yspeed = map(this.z, 0, 20, 1, 20);
-   }
- 
-   fall() {
-     this.y += this.yspeed;
- 
-     let grav = map(this.z, 0, 20, 0, 0.2);
-     this.yspeed += grav;
- 
-     if (this.y > height) {
-       this.y = random(-200, -100);
-       this.yspeed = map(this.z, 0, 20, 1, 20);
-     }
-   }
- 
-   display() {
-     let dropWidth = map(this.z, 0, 20, 1, 4);
-     let dropHeight = map(this.z, 0, 20, 10, 30);
- 
-     noStroke();
-     fill(150, 150, 255);
-     ellipse(this.x, this.y, dropWidth, dropHeight);
-   }
- }
- 
- class Sparkline {
-   constructor() {
-     this.x = random(width);
-     this.y = random(height / 2);
-     this.length = random(20, 100);
-     this.alpha = 255;
-     this.fadeSpeed = random(2, 4);
-   }
- 
-   display() {
-     stroke(50, 50, 255, this.alpha);
-     strokeWeight(2);
-     line(this.x, this.y, this.x + this.length, this.y - this.length);
-     this.alpha -= this.fadeSpeed;
-   }
- 
-   isFinished() {
-     return this.alpha <= 0;
-   }
- }
- 
- function toggleRain() {
+let drops = [];
+let thunderTime = 0;
+let sparklines = [];
+let backgroundBrightness = 0;
+let isRaining = true;
+let rainToggle;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 100; i++) {
+    drops.push(new Drop());
+  }
+
+  rainToggle = document.getElementById('rainToggle');
+  rainToggle.addEventListener('change', toggleRain);
+}
+
+function draw() {
+  background(backgroundBrightness);
+
+  if (isRaining) {
+    for (let i = 0; i < drops.length; i++) {
+      drops[i].fall();
+      drops[i].display();
+    }
+
+    if (millis() - thunderTime > random(5000, 6000)) {
+      thunderTime = millis();
+      sparklines.push(new Sparkline());
+      backgroundBrightness = 100;
+    }
+
+    for (let i = sparklines.length - 1; i >= 0; i--) {
+      sparklines[i].display();
+      if (sparklines[i].isFinished()) {
+        sparklines.splice(i, 1);
+      }
+    }
+
+    backgroundBrightness -= 1;
+  }
+}
+
+class Drop {
+  constructor() {
+    this.x = random(width);
+    this.y = random(-500, -50);
+    this.z = random(0, 20);
+    this.len = map(this.z, 0, 20, 10, 30);
+    this.yspeed = map(this.z, 0, 20, 1, 20);
+  }
+
+  fall() {
+    this.y += this.yspeed;
+
+    let grav = map(this.z, 0, 20, 0, 0.2);
+    this.yspeed += grav;
+
+    if (this.y > height) {
+      this.y = random(-200, -100);
+      this.yspeed = map(this.z, 0, 20, 1, 20);
+    }
+  }
+
+  display() {
+    let dropWidth = map(this.z, 0, 20, 1, 4);
+    let dropHeight = map(this.z, 0, 20, 10, 30);
+
+    noStroke();
+    fill(150, 150, 255);
+    ellipse(this.x, this.y, dropWidth, dropHeight);
+  }
+}
+
+class Sparkline {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height / 2);
+    this.length = random(20, 100);
+    this.alpha = 255;
+    this.fadeSpeed = random(2, 4);
+  }
+
+  display() {
+    stroke(50, 50, 255, this.alpha);
+    strokeWeight(2);
+    line(this.x, this.y, this.x + this.length, this.y - this.length);
+    this.alpha -= this.fadeSpeed;
+  }
+
+  isFinished() {
+    return this.alpha <= 0;
+  }
+}
+
+function toggleRain() {
   isRaining = !isRaining;
 
   if (isRaining) {
@@ -419,7 +447,7 @@ nextButton.addEventListener('click', () => {
   }
 }
 
- 
+
 
 
 // This is for the footer Names 
